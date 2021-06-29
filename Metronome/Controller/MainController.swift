b913f5c2f3176735778fbd76154ec87133ac81d6
Ilya Tereshkin <ilyatereshkin@gmail.com>
2021-06-29 23:39:43 +0300

    Beat levels basis

--- Metronome/Controller/MainController.swift
+++ Metronome/Controller/MainController.swift
@@ -14,6 +14,11 @@ class MainController: UIViewController {
     private var beepTime = 60
     private let INIT_TEMPO = 60
     private var needToUpdateTimer = false
+    private var highStrokeNum = 4
+    private var currentStrokeNum = 0
+    
+    private var playerHighSound : AVAudioPlayer!
+    private var playerLowSound : AVAudioPlayer!
     
     //private var player: AVAudioPlayer
     @IBOutlet weak var tempoLabel: UILabel!
@@ -25,21 +30,78 @@ class MainController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tempoLabel.text = String(INIT_TEMPO)
-        initTempoBtns ()
+        initController ()
     }
     
-    private func initTempoBtns ()
+    private func initController ()
     {
         minusTenTempoBtn.tempo = -10
         minusOneTempoBtn.tempo = -1
         plusOneTempoBtn.tempo = 1
         plusTenTempoBtn.tempo = 10
+        
+        playerHighSound = getAudioPlayer(soundName: "met1", soundExt: "wav")
+        playerLowSound = getAudioPlayer(soundName: "met2", soundExt: "wav")
     }
 
     private func startMetronome ()
     {
         timer.invalidate()
-        let soundUrl = Bundle.main.url(forResource: "met1", withExtension: "wav")
+        updateTimer()
+    }
+    
+    private func StartTimer ()
+    {
+        timer.invalidate()
+        playSound ()
+        updateTimer()
+    }
+    
+    private func updateTimer ()
+    {
+        let interval = 60.0 / Double(beepTime)
+        timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(interval), repeats: false, block: { Timer in
+            self.playSound ()
+            self.updateTimer()
+        })
+    }
+    
+    private func playSound ()
+    {
+        var currPlayer : AVAudioPlayer
+        
+        if (currentStrokeNum == highStrokeNum)
+        {
+            currentStrokeNum = 0
+        }
+        
+        if (currentStrokeNum == 0)
+        {
+            currPlayer = playerHighSound
+        }
+        else
+        {
+            currPlayer = playerLowSound
+        }
+        
+        currPlayer.currentTime = 0
+        if (!currPlayer.isPlaying)
+        {
+            currPlayer.play()
+        }
+        
+        currentStrokeNum += 1
+    }
+    
+    private func updateTempoView ()
+    {
+        tempoLabel.text = String(beepTime)
+    }
+    
+    private func getAudioPlayer (soundName : String, soundExt : String) -> AVAudioPlayer
+    {
+        let soundUrl = Bundle.main.url(forResource: soundName, withExtension: soundExt)
+        
         var player : AVAudioPlayer
 
         do
@@ -49,37 +111,10 @@ class MainController: UIViewController {
         }
         catch
         {
-            print("Error - \(error)")
-            return
+            fatalError("Error - \(error)")
         }
-
-        UpdateTimer(player)
-    }
-    
-    private func StartTimer (_ player : AVAudioPlayer)
-    {
-        timer.invalidate()
-        player.play()
-        UpdateTimer(player)
-    }
-    
-    private func UpdateTimer (_ player : AVAudioPlayer)
-    {
-        let interval = 60.0 / Double(beepTime)
-        timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(interval), repeats: false, block: { Timer in
-            player.currentTime = 0
-            
-            if (!player.isPlaying)
-            {
-                player.play()
-            }
-            self.UpdateTimer(player)
-        })
-    }
-    
-    private func updateTempoView ()
-    {
-        tempoLabel.text = String(beepTime)
+        
+        return player
     }
     
     @IBAction func onStepperChange(_ sender: UIStepper) {

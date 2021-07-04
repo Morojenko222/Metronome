cd2ed8a6a5694fe249d3b86bdc196b8ebe7ca9fc
Ilya Tereshkin <ilyatereshkin@gmail.com>
2021-07-05 00:22:13 +0300

    Data singleton

---
+++ Metronome/Controller/DataContainer.swift
@@ -0,0 +1,16 @@
+//
+//  DataContainer.swift
+//  Metronome
+//
+//  Created by Ilya Tereshkin on 05.07.2021.
+//
+
+import Foundation
+
+struct DataContainer {
+    
+    static var Instance = DataContainer()
+    
+    let sizeData_1 = [1, 2, 3, 4, 5, 6, 7, 8]
+    let sizeData_2 = [1, 2, 3, 4, 5, 6, 7, 8]
+}
--- Metronome/Controller/SizeViewController.swift
+++ Metronome/Controller/SizeViewController.swift
@@ -11,7 +11,8 @@ class SizeViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
     
-    var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    let dataContainer = DataContainer.Instance
+    var activeData = [Int]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,14 +20,15 @@ class SizeViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.tableFooterView = UIView()
-        //self.preferredContentSize = CGSize(width: 250, height: 250)
+        print("DidLoad")
+        activeData = dataContainer.sizeData_1
     }
 }
 
 extension SizeViewController : UITableViewDelegate
 {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Cell name = \(data[indexPath.row])")
+        print("Cell name = \(dataContainer.sizeData_1[indexPath.row])")
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
@@ -34,12 +36,12 @@ extension SizeViewController : UITableViewDelegate
 extension SizeViewController : UITableViewDataSource
 {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return activeData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SizeCell", for: indexPath)
-        cell.textLabel?.text = String(data[indexPath.row])
+        cell.textLabel?.text = String(activeData[indexPath.row])
         return cell
         
     }
